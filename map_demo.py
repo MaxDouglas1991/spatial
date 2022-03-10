b1438525e0110ce5d446e390185ee07c3f08e73b
MaxDouglas1991 <maxdouglas@live.com>
2022-03-10 15:17:34 -0800

    0.1.0

--- map_demo.py
+++ map_demo.py
@@ -2,43 +2,31 @@ from flask import Flask, render_template
 import json
 import requests
 
+# Instructions:
 # export FLASK_APP=map_demo.py
 # flask run
-# 127.0.0.1:5000/
+# Step 1: 127.0.0.1:5000/poi
+# Step 2: 127.0.0.1:5000/map
+# For funzies: 127.0.0.1:5000/lat&lon
+
 app = Flask(__name__)
-
-
-def get_address():
-    # my_address = 'street=206 Concordia Place'
-    my_address = 'street=1342 Hillside Ave'
-
-    url = f'https://nominatim.openstreetmap.org/search/{my_address}?format=json'
-    # url = f'https://nominatim.openstreetmap.org/lookup/{my_address}?format=json'
-    print(url)
-
-    response = requests.get(url).json()
-    print(response)
-    if response:
-        if 'lat' in response[0] and 'lon' in response[0]:
-            print(response[0]["lat"])
-            print(response[0]["lon"])
 
 
 @app.route('/poi', methods=['GET'])
 def get_poi():
-    lang, long = [49.161071, -123.974091]
-    return json.dumps([lang, long]), 200
+    lat, lon = [48.450360, -123.506200]
+    return json.dumps([lat, lon]), 200
 
 
 @app.route('/map', methods=['GET'])
 def display_poi():
-    return render_template('index.html', coord=[49.1615323, -123.97471761]), 200
+    response = requests.get('http://127.0.0.1:5000/poi').json()
+    return render_template('index.html', coord=response), 200
 
 
 @app.route('/<lat>&<lon>', methods=['GET'])
 def get_place(lat, lon):
-    return render_template('index.html', coord=[float(lat), float(lon)]), 200
-
-
-# response = requests.get('127.0.0.1:5000/poi').json()
-# print(response)
+    try:
+        return render_template('index.html', coord=[float(lat), float(lon)]), 200
+    except ValueError:
+        return 'Invalid Coordinates', 200
